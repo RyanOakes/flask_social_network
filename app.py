71d41c03d0e24c33dd565b0ed31a7af4fef48db5
oakes <oakesonline@gmail.com>
2016-11-06 15:25:57 -0500

    Added register/index routes

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, g
+from flask import (Flask, g, render_template, flash, redirect, url_for)
 from flask_login import LoginManager
 
+import forms
 import models
+
 
 DEBUG = True
 # PORT = 8000
@@ -36,8 +38,23 @@ def after_request(response):
     return response
 
 
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = forms.RegisterForm()
+    if form.validate_on_submit():
+        flash('Yay, valid form submitted - you registered', 'success')
+        models.User.create_user(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data
+        )
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
 
 
+@app.route('/'):
+def index():
+    return 'Hi'  
 
 if __name__ == '__main__':
     # app.run(debug=DEBUG, host=HOST, port=PORT)
